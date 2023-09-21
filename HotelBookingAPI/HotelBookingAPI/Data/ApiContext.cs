9e150ce79ee372dc44c9f3e2fe6daf25d2d63ce1
Humna Ghaffar <88514009+humna567@users.noreply.github.com>
2023-09-21 13:20:37 +0500

    Add files via upload

---
+++ HotelBookingAPI/HotelBookingAPI/Data/ApiContext.cs
@@ -0,0 +1,19 @@
+﻿
+using HotelBookingAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace HotelBookingAPI.Data
+{
+    public class ApiContext : DbContext
+    {
+
+        public DbSet<HotelBooking> Bookings { get; set; }
+
+        public ApiContext(DbContextOptions<ApiContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
